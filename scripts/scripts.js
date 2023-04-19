bce9fb4b780dbb57ca3f9e3c1b81e618dc0ba83f
Kristi Li <kli1226@bu.edu>
2023-04-19 11:35:22 -0400

    Added the checkbox Javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,10 @@
-/* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+function checkBox() {
+    var checkBox = document.getElementById("card_tomato");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+       text.style.display = "block";
+       <p id="text" style="display:none">Tomato</p>
+    } else {
+       text.style.display = "none";
+    }
+}
